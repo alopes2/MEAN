26a45a6c1cd42b2e0fbf999d7a647475e78d8abe
alopes2 <andrevitorlopes@gmail.com>
2017-07-20 22:10:50 -0300

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -2,10 +2,17 @@
 console.log(process.env);
 module.exports = {
     env: 'production',
-    db: process.env.OPENSHIFT_MONGODB_DB_URL + 'contatooh',
+    db: 'mongodb://' + 
+        process.env.MONGODB_USER + ':' +
+        process.env.MONGODB_PASSWORD + '@' +
+        process.env.MONGODB_SERVICE_HOST + ':' +
+        process.env.MONGODB_SERVICE_PORT + '/' +
+        'contatooh',
+        //process.env.OPENSHIFT_APP_NAME;
+        //,OPENSHIFT_MONGODB_DB_URL + 'contatooh',
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    port: process.env.OPENSHIFT_NODEJS_PORT,
-    address: process.env.OPENSHIFT_NODEJS_IP,
+    port: process.env.CONTATOOH_SERVICE_PORT,//OPENSHIFT_NODEJS_PORT
+    address: process.env.CONTATOOH_SERVICE_HOST,//OPENSHIFT_NODEJS_IP
     domain: process.env.OPENSHIFT_APP_DNS
     };
